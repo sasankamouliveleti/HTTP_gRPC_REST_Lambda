8b05b2f20098764f380d4b30610e49a21945f196
Sasanka Mouli Veleti <svelet2@uic.edu>
2022-10-28 09:04:58 -0500

    Added test file

---
+++ gRPC_Rest/src/test/scala/gRPCLambdaHttpTester.scala
@@ -0,0 +1,16 @@
+package scala
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import HelperUtils.Constants
+import com.typesafe.config.ConfigFactory
+
+class MRTesterClass extends AnyFlatSpec with Matchers {
+  behavior of "Various helper methods in constants file"
+
+  it should "should get correct timeInterval from Input Time for a timeInterval of 1" in {
+    val inputTime = "14:23".split(":")
+    val testVal = Constants.generateTimeInterval(inputTime)
+    "14:23 14:24" shouldBe testVal
+  }
+}

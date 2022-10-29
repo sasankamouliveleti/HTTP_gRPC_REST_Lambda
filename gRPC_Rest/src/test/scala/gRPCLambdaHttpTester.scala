1e9c602f6ea801043b126fc52677ad276be3a757
Sasanka Mouli Veleti <svelet2@uic.edu>
2022-10-29 14:42:52 -0500

    Added test file

--- gRPC_Rest/src/test/scala/gRPCLambdaHttpTester.scala
+++ gRPC_Rest/src/test/scala/gRPCLambdaHttpTester.scala
@@ -3,14 +3,29 @@ package scala
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import HelperUtils.Constants
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 
 class gRPCLambdaHttpTester extends AnyFlatSpec with Matchers {
-  behavior of "Various helper methods in constants file"
+  behavior of "Various Constants and Methods"
 
-  it should "should get correct timeInterval from Input Time for a timeInterval of 1" in {
-    val inputTime = "14:23".split(":")
-    //val testVal = Constants.generateTimeInterval(inputTime)
-    //"14:23 14:24" shouldBe testVal
+  it should "get correct defined in application config" in {
+    val getUrl = Constants.lambdaEndpoint
+    val config: Config = ConfigFactory.load("application.conf").getConfig("applicationConfigParams")
+    val lambdaEndpoint: String = config.getString("lambdaEndpoint")
+    getUrl shouldBe lambdaEndpoint
   }
+
+  it should "get correct port for gRPC connected in application config" in {
+    val portval: Int = Constants.portNumber
+    val config: Config = ConfigFactory.load("application.conf").getConfig("applicationConfigParams")
+    val port: Int = config.getInt("portNumber")
+    portval shouldBe port
+  }
+
+//  it should "get correct port for gRPC connected in application config" in {
+//    val portval = Constants.portNumber
+//    val config: Config = ConfigFactory.load("application.conf").getConfig("applicationConfigParams")
+//    val port: String = config.getString("portNumber")
+//    portval shouldBe port
+//  }
 }
